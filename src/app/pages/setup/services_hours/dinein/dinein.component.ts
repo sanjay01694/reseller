4dadccb08a7e54370fc102c7c1c5a6cc5dc68c2b
unknown <sanjay01694>
2022-12-03 09:36:29 +0530

    code pushed

---
+++ src/app/pages/setup/services_hours/dinein/dinein.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dinein',
+  templateUrl: './dinein.component.html',
+  styleUrls: ['./dinein.component.scss']
+})
+export class DineinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
